3e9e98b05f8ac0232667831b66a5bed48bde36c2
Siddharth Pande <sidarjunstn.sp@gmail.com>
2020-07-12 08:53:28 +0530

    minor adjustment to comment in the html file.

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const containerDiv = document.querySelector('#container');
 
 let count = 1;
 
-while (count <= 256) { //inserted 16 div in the container Div//
+while (count <= 256) { //inserted 256 div in the container Div//
     const div = document.createElement(`div`);
     div.id = `sq${count}`;
     div.classList.add("childDiv");
